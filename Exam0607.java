8f533650f8716f689e567e8f4ffe92054e934361
bellasylim <74843281+bellasylim@users.noreply.github.com>
2022-03-29 11:47:17 +0900

    Update Exam0607.java

--- Exam0607.java
+++ Exam0607.java
@@ -3,12 +3,12 @@ package Exercise06_220328;
 public class Exam0607 {
     public static void main(String[] args){
         //산의 단수를 나타내는 변수
-        int level = 5;
+        int level = 10;
         //for문 시작
         for(int i = 1; i <= level; i++){
-            String white = "□ ";
+            String white = "  ";
             String black = "■ ";
-            int j = 5;
+            int j = level;
                 System.out.println(white.repeat(j - i) + black.repeat(i + (i - 1)));
             }
         }
